41fb62920b4e0e2f47cd99c50d4b6e7af2ca4679
Ludiamen <maxvas1001@gmail.com>
2024-09-21 13:52:35 +0300

    improve code to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import { useRef, useState } from 'react'
-import './App.css'
-import { Form } from './components/Form/Form'
-import { Watch } from './components/Watch/Watch'
-import { nanoid } from 'nanoid'
+import { useState } from 'react';
+import './App.css';
+import { Form } from './components/Form/Form';
+import { Watch } from './components/Watch/Watch';
+import { nanoid } from 'nanoid';
 
 function App() {
-  const [form, setForm] = useState({id: '', city: '', offset: ''})
-  const clocks = useRef<{id: string, city: string, offset: string}[]>([]);
+  const [form, setForm] = useState({ id: '', city: '', offset: '' });
+  const [clocks, setClocks] = useState<{ id: string, city: string, offset: string }[]>([]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    form.city !== '' && form.offset !== '' && clocks.current.filter(clock => clock.city === form.city).length === 0 ?
-    clocks.current.push({id: nanoid(), city: form.city, offset: form.offset}): '';
-    setForm((prevForm) => ({...prevForm}))
-  }
+    e.preventDefault();
+    if (form.city !== '' && form.offset !== '' && clocks.filter(clock => clock.city === form.city).length === 0) {
+      setClocks((prevClocks) => [...prevClocks, { id: nanoid(), city: form.city, offset: form.offset }]);
+    }
+    setForm({ id: '', city: '', offset: '' }); // Очистим форму после отправки
+  };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setForm((prevForm) => ({...prevForm, [name]: value}))
-  }
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
 
-  const handleOndelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const elem = e.currentTarget.closest('.Clock-box');
-    elem ? clocks.current = clocks.current.filter(clock => clock.id !== elem.id) : '';
-    setForm((prevForm) => ({...prevForm}))
-  }
+  const handleOnDelete = (id: string) => {
+    setClocks((prevClocks) => prevClocks.filter(clock => clock.id !== id));
+  };
 
   return (
     <>
-      <Form changeField={handleOnChange} submit={handleSubmit}/>
-      <div className='watches-box'>
-          {clocks.current.map(clock => <Watch data={clock}  onDelete={handleOndelete} key={clock.id}/>)}
+      <Form changeField={handleOnChange} submit={handleSubmit} />
+      <div className="watches-box">
+        {clocks.map((clock) => (
+          <Watch data={clock} onDelete={() => handleOnDelete(clock.id)} key={clock.id} />
+        ))}
       </div>
-      
     </>
-  )
+  );
 }
 
 export default App
